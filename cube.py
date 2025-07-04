b46bb8a177b6e40882da0ce4faedcb4bdb537d88
Nick Kramer <2532885+nkramer@users.noreply.github.com>
2025-07-03 21:40:58 -0700

    Move all the code out of the notebook

--- cube.py
+++ cube.py
@@ -1,10 +1,122 @@
+# import random
 
-# Online Python - IDE, Editor, Compiler, Interpreter
-import random
+# #x = ["L", "L'", "R", "R'"]
+# x = "L L' R R'".split(" ")
+# path = [random.choice(x) for i in range(20)]
+# print( " ".join(path) )
 
-#x = ["L", "L'", "R", "R'"]
-x = "L L' R R'".split(" ")
-path = [random.choice(x) for i in range(20)]
-print( " ".join(path) )
+# import numpy as np
+
 
 import numpy as np
+# axes = (up/down, left/right, back/front, faces)
+# faces = the colors of (up/down, left/right, back/front)
+
+top    = 0, slice(0,3), slice(0,3), 0 # == cube[0,:,:,0]
+bottom = 2, slice(0,3), slice(0,3), 0 
+left   = slice(0,3), 0, slice(0,3), 1
+right  = slice(0,3), 2, slice(0,3), 1
+front  = slice(0,3), slice(0,3), 0, 2
+back   = slice(0,3), slice(0,3), 2, 2
+
+a = np.zeros(3*3*3*3, dtype='str').reshape([3,3,3,3])
+a[:,:,:,:] = '-'
+a[top] = 'W'
+a[bottom] = 'Y'
+a[left] = 'O'  
+a[right] = 'R' 
+a[front] = 'G' 
+a[back] = 'B'  
+
+# test cube
+letters = [chr(i) for i in range(ord('a'), ord('z') + 1)] + [chr(i) for i in range(ord('A'), ord('Z') + 1)] + [chr(i) for i in range(ord('0'), ord('9') + 1)]
+for i in range(len([top, bottom, left, front, right, back])):
+    face = [top, bottom, left, front, right, back][i]
+    a[face] = np.array(letters[9*i:9*(i+1)]).reshape([3,3])
+
+def p(a=a): # Print the cube
+    blank = np.zeros(3*3, dtype='str').reshape([3,3]); blank[:,:] = ' '
+    strip = np.zeros(3*1, dtype='str').reshape([3,1]); strip[:,:] = ' '
+    c = np.concatenate((
+        np.concatenate((blank, strip, np.flip(np.transpose(a[top]), axis=0), strip, blank, strip, blank), axis=1),
+        np.concatenate((np.flip(a[left], axis=1), strip, a[front], strip, a[right], strip, np.flip(a[back], axis=1)), axis=1),
+        np.concatenate((blank, strip, np.transpose(a[bottom]), strip, blank, strip, blank), axis=1)
+    ))
+    for i in range(c.shape[0]):
+        print(' '.join(c[i,:]) )
+
+def p4(a=a): # Print the 4D hypercube as a 3x3 grid of 3x3 grids
+    blank = np.zeros(3*3 + 2, dtype='str').reshape([1,11]); blank[:,:] = ' '
+    strip = np.zeros(3*1, dtype='str').reshape([3,1]); strip[:,:] = ' '
+    c = np.concatenate((
+        np.concatenate([a[0,0,:,:], strip, a[0,1,:,:], strip, a[0,2,:,:]], axis=1), blank,
+        np.concatenate([a[1,0,:,:], strip, a[1,1,:,:], strip, a[1,2,:,:]], axis=1), blank,
+        np.concatenate([a[2,0,:,:], strip, a[2,1,:,:], strip, a[2,2,:,:]], axis=1)
+    ))
+    for i in range(c.shape[0]):
+        print(' '.join(c[i]) )
+    print('----')
+
+# Rotate the top clockwise
+def rotate_top(cube):
+    b=cube.copy()
+    b[0,:,:,:]=np.rot90(b[0,:,:,:], axes=[0, 1], k=-1)
+    #p4(b)
+    c=b.copy()
+    c[0,:,:,1] = b[0,:,:,2]
+    c[0,:,:,2] = b[0,:,:,1]
+    return c
+
+def reorient(cube, newTop):
+    ax=[0,1,2,3]
+    roll = 0
+    k=1
+    match newTop:
+        case 'U': ax=[0,1]; k=0; roll=0
+        case 'D': ax=[1,0]; k=2; roll=0
+        case 'L': ax=[1,0]; k=1; roll=-1
+        case 'R': ax=[1,0]; k=3; roll=-1
+        case 'F': ax=[2,0]; k=1; roll=-1
+        case 'B': ax=[2,0]; k=3; roll=-1
+    #print(cube[0,0,0,:], cube[2,2,2,:])
+    cube = np.rot90(cube, k=k, axes=ax)
+    #p(cube)
+    old = cube.copy()
+    cube = cube.copy()
+    match newTop:  # Reorient the fourth/faces dimension
+        case 'L' | 'R': 
+            cube[:,:,:,0] = old[:,:,:,1]
+            cube[:,:,:,1] = old[:,:,:,0]
+        case 'F' | 'B': 
+            cube[:,:,:,0] = old[:,:,:,2]
+            cube[:,:,:,2] = old[:,:,:,0]
+    #p(cube)
+    #print(cube[2,2,2,:])
+    return cube
+# axes = (up/down, left/right, back/front, faces)
+# faces = the colors of (up/down, left/right, back/front)
+reorient(a, 'B')
+
+#udlrfb
+#L = Clockwise, L' = counterclockwise
+p(a)
+def perm(face, cube):
+    match face[0]:
+        case 'U': x=('U', 'U')
+        case 'D': x=('D', 'D')
+        case 'L': x=('L', 'R')
+        case 'R': x=('R', 'L')
+        case 'F': x=('F', 'B')
+        case 'B': x=('B', 'F')
+    b=cube.copy()    
+    b = reorient(b, x[0])
+    #p(b)
+    print(x[0])
+    for i in range(1 if len(face) == 1 else 3):
+        b = rotate_top(b)
+        print("hi!")
+    #p(b)
+    b = reorient(b, x[1]) 
+    return b
+
+p(perm("B", a))
