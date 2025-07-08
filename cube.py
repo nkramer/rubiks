34575665a6027adbe80513529082844534ebf0d5
Nick Kramer <2532885+nkramer@users.noreply.github.com>
2025-07-08 16:21:14 -0700

    Refactor a few things to make testing easier

--- cube.py
+++ cube.py
@@ -17,31 +17,37 @@ right  = slice(0,3), 2, slice(0,3), 1
 front  = slice(0,3), slice(0,3), 0, 2
 back   = slice(0,3), slice(0,3), 2, 2
 
-a = np.zeros(3*3*3*3, dtype='str').reshape([3,3,3,3])
-a[:,:,:,:] = '-'
-a[top] = 'W'
-a[bottom] = 'Y'
-a[left] = 'O'  
-a[right] = 'R' 
-a[front] = 'G' 
-a[back] = 'B'  
-# faces = [top, bottom, left, front, right, back]
-# for (face, color) in zip(faces, "WYORGB"):
-#      a[face] = color
+def new_cube():
+    a = np.zeros(3*3*3*3, dtype='str').reshape([3,3,3,3])
+    a[:,:,:,:] = '-'
+    a[top] = 'W'
+    a[bottom] = 'Y'
+    a[left] = 'O'  
+    a[right] = 'R' 
+    a[front] = 'G' 
+    a[back] = 'B'  
+    # faces = [top, bottom, left, front, right, back]
+    # for (face, color) in zip(faces, "WYORGB"):
+    #      a[face] = color
+    return a
 
+a = new_cube()
 
-# test cube
-letters = [chr(i) for i in range(ord('a'), ord('z') + 1)] + [chr(i) for i in range(ord('A'), ord('Z') + 1)] + [chr(i) for i in range(ord('0'), ord('9') + 1)]
-#letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+def new_test_cube():
+    a = new_cube()
+    letters = [chr(i) for i in range(ord('a'), ord('z') + 1)] + [chr(i) for i in range(ord('A'), ord('Z') + 1)] + [chr(i) for i in range(ord('0'), ord('9') + 1)]
+    #letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-for i in range(len([top, bottom, left, front, right, back])):
-    face = [top, bottom, left, front, right, back][i]
-    a[face] = np.array(letters[9*i:9*(i+1)]).reshape([3,3])
-# for i in range(len(faces)):
-#     a[faces[i]] = np.array(letters[9*i:9*(i+1)]).reshape([3,3])
+    for i in range(len([top, bottom, left, front, right, back])):
+        face = [top, bottom, left, front, right, back][i]
+        a[face] = np.array(letters[9*i:9*(i+1)]).reshape([3,3])
+    # for i in range(len(faces)):
+    #     a[faces[i]] = np.array(letters[9*i:9*(i+1)]).reshape([3,3])
+    return a
 
+a = new_test_cube()
 
-def p(a): # Print the cube
+def cube_to_string(a): # Convert cube to string representation
     blank = np.zeros(3*3, dtype='str').reshape([3,3]); blank[:,:] = ' '  # blank grid
     strip = np.zeros(3*1, dtype='str').reshape([3,1]); strip[:,:] = ' '  # blank column
     c = np.concatenate((
@@ -49,10 +55,12 @@ def p(a): # Print the cube
         np.concatenate((np.flip(a[left], axis=1), strip, a[front], strip, a[right], strip, np.flip(a[back], axis=1)), axis=1),
         np.concatenate((blank, strip, np.transpose(a[bottom]), strip, blank, strip, blank), axis=1)
     ))
-    for i in range(c.shape[0]):
-        print(' '.join(c[i,:]) )
+    return '\n'.join([' '.join(c[i,:]) for i in range(c.shape[0])])
 
-def p4(a): # Print the 4D hypercube as a 3x3 grid of 3x3 grids
+def p(a): # Print the cube
+    print(cube_to_string(a))
+
+def cube_to_4d_string(a): # Convert 4D hypercube to string representation
     row = np.zeros(3*3 + 2, dtype='str').reshape([1,11]); row[:,:] = ' '   # blank row
     col = np.zeros(3*1, dtype='str').reshape([3,1]); col[:,:] = ' '        # blank column
     c = np.concatenate((
@@ -60,8 +68,10 @@ def p4(a): # Print the 4D hypercube as a 3x3 grid of 3x3 grids
         np.concatenate([a[1,0,:,:], col, a[1,1,:,:], col, a[1,2,:,:]], axis=1), row,
         np.concatenate([a[2,0,:,:], col, a[2,1,:,:], col, a[2,2,:,:]], axis=1)
     ))
-    for i in range(c.shape[0]):
-        print(' '.join(c[i]) )
+    return '\n'.join([' '.join(c[i]) for i in range(c.shape[0])])
+
+def p4(a): # Print the 4D hypercube as a 3x3 grid of 3x3 grids
+    print(cube_to_4d_string(a))
 
 # Rotate the top clockwise
 def rotate_top(cube):
